c2abf3f11987633ea4137f04cf48f1ed427fc76a
Commissar <fangwei0505@gmail.com>
2017-06-04 01:08:01 +0800

    Increase multilingual support

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	_ "zevcms/routers"
 	"github.com/astaxie/beego"
+	"zevcms/controllers"
+	"github.com/beego/i18n"
 )
 
 func main() {
+	controllers.InitApp()
+
+	beego.Router("/", &controllers.HomeController{})
+	beego.Router("/category", &controllers.CategoryController{})
+	beego.Router("/topic", &controllers.TopicController{})
+	beego.Router("/tags", &controllers.TagsController{})
+	beego.Router("/archives", &controllers.ArchivesController{})
+
+	beego.AddFuncMap("i18n", i18n.Tr)
+
 	beego.Run()
 }
-
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
-alert("hello")
+$(function () {
+    //语言切换
+    $(document).on('click', '.lang-changed', function () {
+        var $e = $(this);
+        var lang = $e.data('lang');
+        $.cookie('lang', lang, {path: '/', expires: 365});
+        window.location.reload();
+    });
+})
