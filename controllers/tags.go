583681a9d55633fd41804ea4217b53df2d225eba
Commissar <fangwei0505@gmail.com>
2017-07-01 23:41:52 +0800

    Add tags controller

--- controllers/tags.go
+++ controllers/tags.go
@@ -8,3 +8,18 @@ func (this *TagsController) Get() {
 	this.Data["IsTag"] = true
 	this.TplName = "default/tags.html"
 }
+func (this *TagsController) TagsList()  {
+	this.TplName = "admin/tags/list.html"
+}
+func (this *TagsController) GetTagsList()  {
+	this.TplName = "admin/tags/list.html"
+}
+func (this *TagsController) AddTags()  {
+	this.TplName = "admin/tags/list.html"
+}
+func (this *TagsController) UpdTags()  {
+	this.TplName = "admin/tag/edit.html"
+}
+func (this *TagsController) DelTags()  {
+	
+}
