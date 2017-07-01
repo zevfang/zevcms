519030a5fd68299dd1791cab6eb7c57855b1eaae
Commissar <fangwei0505@gmail.com>
2017-07-02 00:10:53 +0800

    Add tags action function（add，list）

--- controllers/tags.go
+++ controllers/tags.go
@@ -1,5 +1,11 @@
 package controllers
 
+import (
+	"zevcms/models"
+	"github.com/astaxie/beego"
+	"fmt"
+)
+
 type TagsController struct {
 	BaseController
 }
@@ -8,18 +14,39 @@ func (this *TagsController) Get() {
 	this.Data["IsTag"] = true
 	this.TplName = "default/tags.html"
 }
-func (this *TagsController) TagsList()  {
+
+func (this *TagsController) TagsList() {
 	this.TplName = "admin/tags/list.html"
+	tags, err := models.GetTagsList()
+	if err != nil {
+		beego.Error(err)
+		return
+	}
+	this.Data["TagsList"] = tags
 }
-func (this *TagsController) GetTagsList()  {
-	this.TplName = "admin/tags/list.html"
+
+func (this *TagsController) AddTags() {
+
+	if this.Ctx.Request.Method == "POST" {
+		fmt.Println("123")
+		tagname := this.Input().Get("TagName")
+		if len(tagname) == 0 {
+			this.Redirect("/admin/tags/list", 302)
+			return
+		}
+		err:=models.AddTags(tagname)
+		if err != nil {
+			beego.Error(err)
+			return
+		}
+
+	}
+	this.Redirect("/admin/tags/list", 302)
 }
-func (this *TagsController) AddTags()  {
-	this.TplName = "admin/tags/list.html"
-}
-func (this *TagsController) UpdTags()  {
+
+func (this *TagsController) UpdTags() {
 	this.TplName = "admin/tag/edit.html"
 }
-func (this *TagsController) DelTags()  {
-	
+func (this *TagsController) DelTags() {
+
 }
--- models/tags_model.go
+++ models/tags_model.go
@@ -1,10 +1,28 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/astaxie/beego/orm"
+)
 
 type Tags struct {
-	Id         int64 `orm:"index"`
+	Id         int64 `orm:"pk;auto"`
 	TagName    string
-	TopicCount int64
-	Created    time.Time `orm:"index"`
+	TopicCount int64 `orm:"default(0)"`
+	Created    time.Time `orm:"auto_now_add;type(datetime)"`
+}
+
+func GetTagsList() ([]*Tags, error) {
+	o := orm.NewOrm()
+	tags := make([]*Tags, 0)
+	qt := o.QueryTable("tags")
+	_, err := qt.All(&tags)
+	return tags, err
+}
+
+func AddTags(tagname string) error {
+	tag := &Tags{TagName: tagname}
+	o := orm.NewOrm()
+	_, err := o.Insert(tag)
+	return err
 }
